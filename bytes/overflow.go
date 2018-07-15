e7b349a52510136375ae289ce2dd9ad18642bbbc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-15 07:49:55 -0700

    convbyte.Overflow

---
+++ bytes/overflow.go
@@ -0,0 +1,27 @@
+package convbytes
+
+import (
+	"fmt"
+)
+
+type Overflow struct {
+	value   string
+	reason  string
+	context string
+}
+
+func (receiver Overflow) Error() string {
+	return fmt.Sprintf("conv: overflow caused by %q: %s", receiver.value, receiver.reason)
+}
+
+func (receiver Overflow) Context() string {
+	return receiver.context
+}
+
+func (receiver Overflow) Reason() string {
+	return receiver.reason
+}
+
+func (receiver Overflow) Value() string {
+	return receiver.value
+}
