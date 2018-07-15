521cb2f41b2466748b855ed606a4471bb9eeb115
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-15 07:43:20 -0700

    improved convbytes.InterpretationError

--- bytes/interpretationerror.go
+++ bytes/interpretationerror.go
@@ -5,10 +5,23 @@ import (
 )
 
 type InterpretationError struct {
-	value  string
-	reason string
+	value   string
+	reason  string
+	context string
 }
 
 func (receiver InterpretationError) Error() string {
 	return fmt.Sprintf("conv: interpretation error of %q: %s", receiver.value, receiver.reason)
+}
+
+func (receiver InterpretationError) Context() string {
+	return receiver.context
+}
+
+func (receiver InterpretationError) Reason() string {
+	return receiver.reason
+}
+
+func (receiver InterpretationError) Value() string {
+	return receiver.value
 }
