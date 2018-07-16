54ee9a15b15afb342ae5351d3e8bd6bb35ed6576
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-16 15:42:09 -0700

    convwrite.Bool()

--- writer/bool.go
+++ writer/bool.go
@@ -4,14 +4,139 @@ import (
 	"io"
 )
 
-func Bool(writer io.Writer, value bool) (int64, error) {
+var (
+	boolStyle_0_1_false = [...]byte{'0'}
+	boolStyle_0_1_true  = [...]byte{'1'}
+
+	boolStyle_F_T_false = [...]byte{'F'}
+	boolStyle_F_T_true  = [...]byte{'T'}
+
+	boolStyle_f_t_false = [...]byte{'f'}
+	boolStyle_f_t_true  = [...]byte{'t'}
+
+	boolStyle_up_down_false = []byte("⊥")
+	boolStyle_up_down_true  = []byte("⊤")
+
+	boolStyle_OFF_ON_false = [...]byte{'O','F','F'}
+	boolStyle_OFF_ON_true  = [...]byte{'O','N'}
+
+	boolStyle_off_on_false = [...]byte{'o','f','f'}
+	boolStyle_off_on_true  = [...]byte{'o','n'}
+
+	boolStyle_Off_On_false = [...]byte{'O','f','f'}
+	boolStyle_Off_On_true  = [...]byte{'O','n'}
+
+	boolStyle_NO_YES_false = [...]byte{'N','O'}
+	boolStyle_NO_YES_true  = [...]byte{'Y','E','S'}
+
+	boolStyle_no_yes_false = [...]byte{'n','o'}
+	boolStyle_no_yes_true  = [...]byte{'y','e','s'}
+
+	boolStyle_No_Yes_false = [...]byte{'N','o'}
+	boolStyle_No_Yes_true  = [...]byte{'Y','e','s'}
+
+	boolStyle_FALSE_TRUE_false = [...]byte{'F','A','L','S','E'}
+	boolStyle_FALSE_TRUE_true  = [...]byte{'T','R','U','E'}
+
+	boolStyle_default_false = [...]byte{'f','a','l','s','e'}
+	boolStyle_default_true  = [...]byte{'t','r','u','e'}
+
+	boolStyle_False_True_false = [...]byte{'F','a','l','s','e'}
+	boolStyle_False_True_true  = [...]byte{'T','r','u','e'}
+)
+
+// Bool converts a bool into a human readable form.
+//
+// For example:
+//
+//	var writer io.Writer
+//
+//	//...
+//
+//	var value bool
+//	
+//	//...
+//	
+//	convwrite.Bool(writer, value)
+func Bool(writer io.Writer, value bool, params ...interface{}) (int64, error) {
 
 	if nil == writer {
 		return 0, errNilWriter
 	}
 
-	var t [4]byte = [4]byte{'t','r','u','e'}
-	var f [5]byte = [5]byte{'f','a','l','s','e'}
+	var f []byte
+	var t []byte
+	{
+		var style interface{}
+
+		if 0 < len(params) {
+			param0 := params[0]
+
+			style = param0
+		}
+
+
+		switch style {
+		case '0','1',
+		     "0","1":
+			f = boolStyle_0_1_false[:]
+			t = boolStyle_0_1_true[:]
+
+		case 'F','T',
+		     "F","T":
+			f = boolStyle_F_T_false[:]
+			t = boolStyle_F_T_true[:]
+
+
+		case 'f','t',
+		     "f","t":
+			f = boolStyle_f_t_false[:]
+			t = boolStyle_f_t_true[:]
+
+		case '⊥','⊤',
+		     "⊥","⊤":
+			f = boolStyle_up_down_false
+			t = boolStyle_up_down_true
+
+		case "OFF","ON":
+			f = boolStyle_OFF_ON_false[:]
+			t = boolStyle_OFF_ON_true[:]
+
+		case "off","on":
+			f = boolStyle_off_on_false[:]
+			t = boolStyle_off_on_true[:]
+
+		case "Off","On":
+			f = boolStyle_Off_On_false[:]
+			t = boolStyle_Off_On_true[:]
+
+		case "NO","YES":
+			f = boolStyle_NO_YES_false[:]
+			t = boolStyle_NO_YES_true[:]
+
+		case "no","yes":
+			f = boolStyle_no_yes_false[:]
+			t = boolStyle_no_yes_true[:]
+
+		case "No","Yes":
+			f = boolStyle_No_Yes_false[:]
+			t = boolStyle_No_Yes_true[:]
+
+		case "FALSE","TRUE":
+			f = boolStyle_FALSE_TRUE_false[:]
+			t = boolStyle_FALSE_TRUE_true[:]
+
+		case "False","True":
+			f = boolStyle_False_True_false[:]
+			t = boolStyle_False_True_true[:]
+
+		default:
+			f = boolStyle_default_false[:]
+			t = boolStyle_default_true[:]
+		}
+	}
+
+
 
 	var n64 int64
 	var err error
@@ -19,11 +144,11 @@ func Bool(writer io.Writer, value bool) (int64, error) {
 	switch value {
 	case true:
 		var n int
-		n, err = writer.Write(t[:])
+		n, err = writer.Write(t)
 		n64 = int64(n)
 	default: // case false
 		var n int
-		n, err = writer.Write(f[:])
+		n, err = writer.Write(f)
 		n64 = int64(n)
 	}
 
--- writer/bool_test.go
+++ writer/bool_test.go
@@ -9,6 +9,7 @@ import (
 func TestBool(t *testing.T) {
 
 	tests := []struct{
+		Params   []interface{}
 		Value    bool
 		Expected string
 	}{
@@ -20,6 +21,422 @@ func TestBool(t *testing.T) {
 			Value:     true,
 			Expected: "true",
 		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"FALSE"},
+			Value:                 false,
+			Expected:             "FALSE",
+		},
+		{
+			Params: []interface{}{"FALSE"},
+			Value:                 true,
+			Expected:             "TRUE",
+		},
+
+
+
+		{
+			Params: []interface{}{"TRUE"},
+			Value:                 false,
+			Expected:             "FALSE",
+		},
+		{
+			Params: []interface{}{"TRUE"},
+			Value:                 true,
+			Expected:             "TRUE",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"false"},
+			Value:                 false,
+			Expected:             "false",
+		},
+		{
+			Params: []interface{}{"false"},
+			Value:                 true,
+			Expected:             "true",
+		},
+
+
+
+		{
+			Params: []interface{}{"true"},
+			Value:                 false,
+			Expected:             "false",
+		},
+		{
+			Params: []interface{}{"true"},
+			Value:                 true,
+			Expected:             "true",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"False"},
+			Value:                 false,
+			Expected:             "False",
+		},
+		{
+			Params: []interface{}{"False"},
+			Value:                 true,
+			Expected:             "True",
+		},
+
+
+
+		{
+			Params: []interface{}{"True"},
+			Value:                 false,
+			Expected:             "False",
+		},
+		{
+			Params: []interface{}{"True"},
+			Value:                 true,
+			Expected:             "True",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{'0'},
+			Value:                 false,
+			Expected:             "0",
+		},
+		{
+			Params: []interface{}{'0'},
+			Value:                 true,
+			Expected:             "1",
+		},
+
+
+
+		{
+			Params: []interface{}{'1'},
+			Value:                 false,
+			Expected:             "0",
+		},
+		{
+			Params: []interface{}{'1'},
+			Value:                 true,
+			Expected:             "1",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{'F'},
+			Value:                 false,
+			Expected:             "F",
+		},
+		{
+			Params: []interface{}{'F'},
+			Value:                 true,
+			Expected:             "T",
+		},
+
+
+
+		{
+			Params: []interface{}{'T'},
+			Value:                 false,
+			Expected:             "F",
+		},
+		{
+			Params: []interface{}{'T'},
+			Value:                 true,
+			Expected:             "T",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{'f'},
+			Value:                 false,
+			Expected:             "f",
+		},
+		{
+			Params: []interface{}{'f'},
+			Value:                 true,
+			Expected:             "t",
+		},
+
+
+
+		{
+			Params: []interface{}{'t'},
+			Value:                 false,
+			Expected:             "f",
+		},
+		{
+			Params: []interface{}{'t'},
+			Value:                 true,
+			Expected:             "t",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{'⊥'},
+			Value:                 false,
+			Expected:             "⊥",
+		},
+		{
+			Params: []interface{}{'⊥'},
+			Value:                 true,
+			Expected:             "⊤",
+		},
+
+
+
+		{
+			Params: []interface{}{'⊤'},
+			Value:                 false,
+			Expected:             "⊥",
+		},
+		{
+			Params: []interface{}{'⊤'},
+			Value:                 true,
+			Expected:             "⊤",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"OFF"},
+			Value:                 false,
+			Expected:             "OFF",
+		},
+		{
+			Params: []interface{}{"OFF"},
+			Value:                 true,
+			Expected:             "ON",
+		},
+
+
+
+		{
+			Params: []interface{}{"ON"},
+			Value:                 false,
+			Expected:             "OFF",
+		},
+		{
+			Params: []interface{}{"ON"},
+			Value:                 true,
+			Expected:             "ON",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"off"},
+			Value:                 false,
+			Expected:             "off",
+		},
+		{
+			Params: []interface{}{"off"},
+			Value:                 true,
+			Expected:             "on",
+		},
+
+
+
+		{
+			Params: []interface{}{"on"},
+			Value:                 false,
+			Expected:             "off",
+		},
+		{
+			Params: []interface{}{"on"},
+			Value:                 true,
+			Expected:             "on",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"Off"},
+			Value:                 false,
+			Expected:             "Off",
+		},
+		{
+			Params: []interface{}{"Off"},
+			Value:                 true,
+			Expected:             "On",
+		},
+
+
+
+		{
+			Params: []interface{}{"On"},
+			Value:                 false,
+			Expected:             "Off",
+		},
+		{
+			Params: []interface{}{"On"},
+			Value:                 true,
+			Expected:             "On",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"NO"},
+			Value:                 false,
+			Expected:             "NO",
+		},
+		{
+			Params: []interface{}{"NO"},
+			Value:                 true,
+			Expected:             "YES",
+		},
+
+
+
+		{
+			Params: []interface{}{"YES"},
+			Value:                 false,
+			Expected:             "NO",
+		},
+		{
+			Params: []interface{}{"YES"},
+			Value:                 true,
+			Expected:             "YES",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"no"},
+			Value:                 false,
+			Expected:             "no",
+		},
+		{
+			Params: []interface{}{"no"},
+			Value:                 true,
+			Expected:             "yes",
+		},
+
+
+
+		{
+			Params: []interface{}{"yes"},
+			Value:                 false,
+			Expected:             "no",
+		},
+		{
+			Params: []interface{}{"yes"},
+			Value:                 true,
+			Expected:             "yes",
+		},
+
+
+
+
+
+
+
+
+
+		{
+			Params: []interface{}{"No"},
+			Value:                 false,
+			Expected:             "No",
+		},
+		{
+			Params: []interface{}{"No"},
+			Value:                 true,
+			Expected:             "Yes",
+		},
+
+
+
+		{
+			Params: []interface{}{"Yes"},
+			Value:                 false,
+			Expected:             "No",
+		},
+		{
+			Params: []interface{}{"Yes"},
+			Value:                 true,
+			Expected:             "Yes",
+		},
 	}
 
 
@@ -27,19 +444,19 @@ func TestBool(t *testing.T) {
 
 		var buffer bytes.Buffer
 
-		n64, err := Bool(&buffer, test.Value)
+		n64, err := Bool(&buffer, test.Value, test.Params...)
 		if nil != err {
 			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
 			continue
 		}
 
-		if expected, actual := int64(len(test.Expected)), n64; expected != actual {
-			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+		if expected, actual := test.Expected, buffer.String(); expected != actual {
+			t.Errorf("For test #%d, expected %q, but actually got %q.", testNumber, expected, actual)
 			continue
 		}
 
-		if expected, actual := test.Expected, buffer.String(); expected != actual {
-			t.Errorf("For test #%d, expected %q, but actually got %q.", testNumber, expected, actual)
+		if expected, actual := int64(len(test.Expected)), n64; expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
 			continue
 		}
 	}
